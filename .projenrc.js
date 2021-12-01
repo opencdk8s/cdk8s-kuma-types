aa02148978907ed66df0d706efba726cc7eacdf2
Hunter Thompson <aatman.bos@smallcase.com>
2021-12-01 14:20:41 +0530

    feat: upgrade cdk8s to v1.2.0

--- .projenrc.js
+++ .projenrc.js
@@ -2,9 +2,8 @@ const { ConstructLibraryCdk8s, NpmAccess } = require('projen');
 const project = new ConstructLibraryCdk8s({
   author: 'Hunter Thompson',
   authorAddress: 'aatman@auroville.org.in',
-  cdk8sVersion: '1.0.0-beta.27',
-  cdk8sPlusVersion: '1.0.0-beta.50',
-  constructsVersion: '3.3.120',
+  cdk8sVersion: '1.2.0',
+  constructsVersion: '3.3.161',
   defaultReleaseBranch: 'main',
   name: '@opencdk8s/cdk8s-kuma-types',
   repositoryUrl: 'https://github.com/opencdk8s/cdk8s-kuma-types.git',
@@ -25,9 +24,6 @@ const project = new ConstructLibraryCdk8s({
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
-  peerDeps: [
-    'constructs@^3.3.120',
-  ],
   // packageName: undefined,            /* The "name" in package.json. */
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
